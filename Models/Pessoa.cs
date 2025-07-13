3d51599066b5f7e74205eb14184963dbe16e324b
Lucas Gomes <lucazcode@gmail.com>
2025-07-13 15:50:08 -0300

    Update Pessoa.cs

--- Models/Pessoa.cs
+++ Models/Pessoa.cs
@@ -2,15 +2,12 @@ namespace DesafioProjetoHospedagem.Models;
 
 public class Pessoa
 {
-    public class Pessoa
-    {
-        public string? Nome { get; set; }
-        public string? Sobrenome { get; set; }
+    public string? Nome { get; set; }
+    public string? Sobrenome { get; set; }
 
-        public Pessoa(string nome, string sobrenome)
-        {
-            Nome = nome;
-            Sobrenome = sobrenome;
-        }
+    public Pessoa(string nome, string sobrenome)
+    {
+        Nome = nome;
+        Sobrenome = sobrenome;
     }
 }
